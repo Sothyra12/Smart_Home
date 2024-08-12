74b7b252571c2f577eb3337d7059a6e73e9a2346
ian villanueva <ianemv@gmail.com>
2024-08-12 01:01:20 -0400

    updated unit test. need to login

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -5,9 +5,15 @@ from app.main import app
 client = TestClient(app)
 
 def test_login_success():
-    response = client.post("/login", json={"username": "admin", "password": "secret"})
+    response = client.post("/api/v1/auth/login", json={    
+    "email": "ian2@example.com",
+    "username": "ian2@example.com",
+    "password": "mypassword"
+        })
     assert response.status_code == 200
-    assert response.json() == {"message": "Login successful"}
+    assert response.json().keys() == {"access_token", "token_type", "user"}
+    assert response.json()["token_type"] == "bearer"
+    assert set(response.json()["user"].keys()) == {"username", "email", "user_id"}
 
 def test_login_failure():
     response = client.post("/login", json={"username": "admin", "password": "wrong"})
@@ -30,11 +36,36 @@ def test_auth_endpoint():
     # Add more assertions based on your endpoint's response
 
 def test_rooms_endpoint():
-    response = client.get("/api/v1/rooms")
+    login_response = client.post("/api/v1/auth/login", json={    
+        "email": "ian2@example.com",
+        "username": "ian2@example.com",
+        "password": "mypassword"
+    })
+   
+    assert login_response.status_code == 200
+    
+    # Extract the access token from the login response
+    access_token = login_response.json()["access_token"]
+    
+    headers = {"Authorization": f"Bearer {access_token}"}
+    response = client.get("/api/v1/rooms", headers=headers)
+    
     assert response.status_code == 200
     # Add more assertions based on your endpoint's response
 
 def test_devices_endpoint():
-    response = client.get("/api/v1/devices")
+    login_response = client.post("/api/v1/auth/login", json={    
+        "email": "ian2@example.com",
+        "username": "ian2@example.com",
+        "password": "mypassword"
+    })
+   
+    assert login_response.status_code == 200
+    
+    # Extract the access token from the login response
+    access_token = login_response.json()["access_token"]
+    headers = {"Authorization": f"Bearer {access_token}"}
+
+    response = client.get("/api/v1/devices", headers=headers)
     assert response.status_code == 200
     # Add more assertions based on your endpoint's response
